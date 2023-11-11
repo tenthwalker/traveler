097df586333f66f8ef66742ecdfe49e7aeebe5a5
Arden Ranta <arranta20@gmail.com>
2023-11-11 12:08:35 -0800

    feat/add pseudocode and login function draft

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -1,7 +1,16 @@
 /////////Query Selectors///////
+const loginButton = document.querySelector('#submitLogin');
+const usernameField = document.querySelector('#username');
+const passwordField = document.querySelector('#password');
+const loginView = document.querySelector('#login');
 
 
 ///////Update Functions////////
 
 
-export {}
+export {
+  loginButton,
+  usernameField,
+  passwordField,
+  loginView,
+}
--- src/scriptDefinitions.js
+++ src/scriptDefinitions.js
@@ -1,15 +1,27 @@
 ////////
 
-function loginAttempt(){
+const { usernameField, passwordField } = require("./domUpdates");
 
+function loginAttempt(){
+  if(usernameField.value && passwordField.value === 'travel'){
+    const usernameGiven = usernameField.value;
+
+  } else {
+    alert('Please verify that your username and password are correct before trying again.');
+    usernameField.innerHTML = '';
+    passwordField.innerHTML = '';
+  }
 }
+
+// fetch call for all users on page load
+// grab user ids for all users
 // take in login fields data
+// filter through user ids for all users, check if usernameField.value is equal to 'traveler' + user id 
 // check if username is traveler<id>
 // check if password is travel
 // if both true, addClassList.hidden to the login page and remove it from the dashboard
 // if not, alert('Please verify that your username and password are correct before trying again.)
 // clear out login fields
-
 
 module.exports = {
   loginAttempt,
