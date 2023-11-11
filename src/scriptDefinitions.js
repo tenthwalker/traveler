a1f2dbae0b935272a11f075fda3be466a9523a96
Arden Ranta <arranta20@gmail.com>
2023-11-11 09:28:10 -0800

    feat/pseudocode login function

--- src/scriptDefinitions.js
+++ src/scriptDefinitions.js
@@ -1,5 +1,16 @@
 ////////
 
+function loginAttempt(){
+
+}
+// take in login fields data
+// check if username is traveler<id>
+// check if password is travel
+// if both true, addClassList.hidden to the login page and remove it from the dashboard
+// if not, alert('Please verify that your username and password are correct before trying again.)
+// clear out login fields
 
 
-module.exports = {}
+module.exports = {
+  loginAttempt,
+}
--- src/scripts.js
+++ src/scripts.js
@@ -3,10 +3,15 @@
 ////////////Imports/////////////
 import './css/styles.css';
 import {} from './apiCalls';
-import {} from './scriptDefinitions';
+import {
+  loginAttempt,
+} from './scriptDefinitions';
 import {} from './domUpdates';
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
 ////////Event Listeners/////////
+
+// on click of submit button
+// loginAttempt
 
 
