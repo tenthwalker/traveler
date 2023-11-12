0f74eee6057a405e9aa9789990e09fa33a6e85df
Arden Ranta <arranta20@gmail.com>
2023-11-12 14:28:25 -0800

    feat/add api call for getting all trips

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -13,14 +13,26 @@ function getTravelers() {
   .then((data) => data.travelers)
   .catch(error => {
     console.log(error);
-  })
+  });
 }
 
 ///////GET Traveler by ID////////
 
 
 ////////////GET Trips////////////
-
+function getTrips() {
+  return fetch('http://localhost:3001/api/v1/trips')
+  .then((response) => {
+    if (!response.ok) {
+      throw Error(`Could not get travelers. Request code: ${response.status}`)
+    }
+    return response.json()
+  })
+  .then((data) => data.trips)
+  .catch(error => {
+    console.log(error);
+  });
+}
 
 ////////GET Destinations/////////
 
@@ -36,5 +48,5 @@ function getTravelers() {
 
 export {
   getTravelers,
-  
+
 }
