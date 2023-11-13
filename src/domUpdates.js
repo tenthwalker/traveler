4935f1de7910e3f21073af902f3e428dd8e8f2c8
Arden Ranta <arranta20@gmail.com>
2023-11-13 12:29:47 -0800

    feat/add query selectors

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -3,7 +3,13 @@ const loginButton = document.querySelector('#submitLogin');
 const usernameField = document.querySelector('#username');
 const passwordField = document.querySelector('#password');
 const loginView = document.querySelector('#login');
-const mainView = document.querySelector('#dashboard')
+const mainView = document.querySelector('#dashboard');
+const tripView = document.querySelector('.overview');
+const pastTrips = document.querySelector('#pastTrips');
+const futureTrips = document.querySelector('#futureTrips');
+const pendingTrips = document.querySelector('#pendingTrips');
+const totalSpent = document.querySelector('#totalSpent');
+
 
 ///////Update Functions////////
 
@@ -14,4 +20,9 @@ export {
   passwordField,
   loginView,
   mainView,
+  pendingTrips,
+  pastTrips,
+  futureTrips,
+  tripView,
+  totalSpent
 }
